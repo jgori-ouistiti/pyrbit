943fd7e087cf23713c904c6921245ea02ca487cf
CHI-24-4419 <chi.24.4419@gmail.com>
2023-11-20 14:41:27 +0100

    Bump version: 0.2.0-dev0 → 0.3.0-dev0

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2023, anon"
 author = "Julien Gori"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0-dev0"
+release = "0.3.0-dev0"
 
 
 # -- General configuration ---------------------------------------------------
